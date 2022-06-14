6b5d66300c6da62c1deef144a9d6d6403e40f806
peve <paulovictor237@gmail.com>
2022-06-13 21:58:05 -0300

    Metodos de API

---
+++ README.md
@@ -0,0 +1,9 @@
+# Create a package
+
+```shell
+yarn init -y # inicializar
+yarn add express # adicionar paconte
+yarn add nodemon -D # roda sempre
+node index.js # rodar + yarn dev
+
+```
--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-// //localhost:3333
-app.get('/', (req, res) => {
+
+/**
+ * GET - Buscar uma informação do back-end
+ * POST - Criar uma informação no back-end
+ * PUT - Atualizar uma informação no back-end
+ * PATCH - Atualizar uma informação especifica no back-end
+ * DELETE - Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Request Body: Corpo da requisição, utilizado para criar ou atualizar recursos
+ */
+
+app.get("/courses", (req, res) => {
   // res.send('Hello World!');
-  res.json({
-    message: 'Hello World!'
-  })
+  return res.json(["curso1", "curso2", "curso3"]);
+});
+
+app.post("/courses", (req, res) => {
+  // res.send('Hello World!');
+  return res.json(["curso1", "curso2", "curso3", "curso4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  return res.json(["curso6", "curso2", "curso3", "curso4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["curso6", "curso7", "curso3", "curso4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["curso1", "curso3", "curso4"]);
+});
+
+// //localhost:3333
+app.get("/", (req, res) => {
+  // return res.send('Hello World!');
+  return res.json({ message: "Hello Worldd!" });
 });
 
 app.listen(3333);
